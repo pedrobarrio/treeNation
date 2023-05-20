dcf072637958da4ed908349b09625ab92f98b7ae
pbarrio <pbarrio@tic4you.com>
2023-05-21 01:37:51 +0200

    add Use case

---
+++ src/COTonesCompensation/Application/GetAnnualReport/GetAnnualReportResponse.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+namespace App\COTonesCompensation\Application\GetAnnualReport;
+
+class GetAnnualReportResponse
+{
+
+}
--- src/COTonesCompensation/Domain/COTones.php
+++ src/COTonesCompensation/Domain/COTones.php
@@ -3,15 +3,29 @@ declare(strict_types=1);
 
 namespace App\COTonesCompensation\Domain;
 
+
+use Ramsey\Uuid\Uuid;
+
 class COTones
 {
-    public function __construct(private Uuid $id, private string $month, private string $numberOfTrees, private string $cotCompensation)
+    public function __construct(
+        private Uuid   $id,
+        private string $year,
+        private string $month,
+        private string $numberOfTrees,
+        private string $cotCompensation
+    )
     {
     }
 
     public function getId(): Uuid
     {
         return $this->id;
+    }
+
+    public function getYear(): string
+    {
+        return $this->year;
     }
 
     public function getMonth(): string
